6fc0a0803ddc6e2565bc61d2731a8dfba22d5207
PedroAfonso0102 <pedro.app1@proton.me>
2026-04-26 00:25:59 -0300

    aaaaa

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/recom-final-1/', // Ensures correct paths for GitHub Pages subfolder
-})
